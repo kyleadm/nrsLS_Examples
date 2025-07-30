e04e45dd69da12bca264b980c3868c12bf3a0fd5
Kyle Damm <kyle.anthony.damm@gmail.com>
2025-07-30 17:47:15 +0100

    Added a run and clean script to linear example.

---
+++ linear1D/clean.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+rm *~ *re2 *log *.f* *nek5000
+rm -r .cache
---
+++ linear1D/run_on_local.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+ulimit -s unlimited
+
+export NEKRS_HOME=~/work/software/nekrs_LS_build/nekRS_LS/nekRS/
+
+mpirun --mca osc ucx -np 1 nekrs --setup linear
