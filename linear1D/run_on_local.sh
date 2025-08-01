78967bc04f4a2185bba9a57895224590f5b7b7bd
Kyle Damm <kyle.anthony.damm@gmail.com>
2025-08-01 07:58:50 +0100

    Write out a logfile when running a simulation.

--- linear1D/run_on_local.sh
+++ linear1D/run_on_local.sh
@@ -4,4 +4,4 @@ ulimit -s unlimited
 
 export NEKRS_HOME=~/work/software/nekrs_LS_build/nekRS_LS/nekRS/
 
-mpirun --mca osc ucx -np 1 nekrs --setup linear
+mpirun --mca osc ucx -np 1 nekrs --setup linear | tee log.dat
